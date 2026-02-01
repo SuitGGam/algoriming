1938ac48e258da2dac5f5f3e062242a37e8c4dbb
SuitGGam <gmj49@naver.com>
2026-02-01 21:54:10 +0900

    chore: 폴더 구조 개선과 파일 최신화를 위한 삭제

--- group-copy/check_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('problemworkbooks_all_pages_edit.csv', encoding='utf-8-sig')
-print(len(df))   # 행 개수 출력(헤더 제외)
-print(df.tail()) # 마지막 5개 행 내용 확인
--- group-copy/check_encoding_type.py
+++
@@ -1,6 +0,0 @@
-import chardet
-
-with open('problemworkbooks_all_pages_edit.csv', 'rb') as f:
-    rawdata = f.read(10000)  # 샘플 읽기
-    result = chardet.detect(rawdata)
-    print(result)
